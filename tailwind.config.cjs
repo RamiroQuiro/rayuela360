15e854447705e356d5280178e1b5a7b84d59728d
ramiroquiro <rama.exe.13@gmail.com>
2023-09-17 19:12:42 -0300

    implementanod servcios

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,24 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    
-    content: ["./src/**/*.{html,js,astro,jsx}"],
+
+  content: ["./src/**/*.{html,js,astro,jsx}"],
   theme: {
     extend: {
+      colors: {
+        primary:{
+        100: '#89CFF0',
+        200: '#98FF98',
+        'text': '#595959',
+        'resaltado': '#FFFACD',
+        'fondo': '#FFD1DC50',
+        'error':"#FF7F50"
+      }},
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
-      keyframe:{
-        
+      keyframe: {
+
         aparecer: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
